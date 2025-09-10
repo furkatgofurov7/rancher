8647b8e803c82f356815225a583a48ac745955b3
furkatgofurov7 <40443040+furkatgofurov7@users.noreply.github.com>
2025-09-10 09:15:01 +0000

    Bump turtles to 107.0.1+up0.19.0-rc.1 (no chart bump)

--- pkg/buildconfig/constants.go
+++ pkg/buildconfig/constants.go
@@ -9,6 +9,6 @@ const (
 	FleetVersion             = "107.0.1+up0.13.1"
 	ProvisioningCAPIVersion  = "107.0.0+up0.8.0"
 	RemoteDialerProxyVersion = "106.0.1+up0.5.0"
-	TurtlesVersion           = "107.0.1+up0.19.0-rc.0"
+	TurtlesVersion           = "107.0.1+up0.19.0-rc.1"
 	WebhookVersion           = "107.0.1+up0.8.1"
 )
